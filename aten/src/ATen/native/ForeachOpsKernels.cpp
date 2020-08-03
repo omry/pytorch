1b81b69d99cccbc06346f99752b2c6c2c72d057d
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-03 10:09:24 -0700

    Added binary ops for scalars

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -1,6 +1,72 @@
 #include <ATen/ATen.h>
 namespace at { namespace native {
 
+std::vector<Tensor> foreach_sub_scalar_kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors.size(); i++) {
+    auto temp = tensors[i].sub(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_sub_scalar__kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  for (int i = 0; i < tensors.size(); i++) {
+    tensors[i].sub_(scalar);
+  }
+
+  return tensors.vec();
+}
+
+std::vector<Tensor> foreach_div_scalar_kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors.size(); i++) {
+    auto temp = tensors[i].div(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_div_scalar__kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  for (int i = 0; i < tensors.size(); i++) {
+    tensors[i].div_(scalar);
+  }
+
+  return tensors.vec();
+}
+
+std::vector<Tensor> foreach_mul_scalar_kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors.size(); i++) {
+    auto temp = tensors[i].mul(scalar);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<Tensor> foreach_mul_scalar__kernel_cpu(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  for (int i = 0; i < tensors.size(); i++) {
+    tensors[i].mul_(scalar);
+  }
+
+  return tensors.vec();
+}
+
 std::vector<Tensor> foreach_add_scalar_kernel_cpu(TensorList tensors, Scalar scalar) {
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
 
@@ -9,6 +75,7 @@ std::vector<Tensor> foreach_add_scalar_kernel_cpu(TensorList tensors, Scalar sca
     auto temp = tensors[i].add(scalar);
     result.emplace_back(temp);
   }
+
   return result;
 }
 
@@ -31,6 +98,7 @@ std::vector<Tensor> foreach_add_list_kernel_cpu(TensorList tensors1, TensorList 
     auto temp = tensors1[i].add(tensors2[i]);
     result.emplace_back(temp);
   }
+
   return result;
 }
 
@@ -41,6 +109,7 @@ std::vector<Tensor> foreach_add_list__kernel_cpu(TensorList tensors1, TensorList
   for (int i = 0; i < tensors1.size(); i++) {
     tensors1[i].add_(tensors2[i]);
   }
+
   return tensors1.vec();
 }
 
